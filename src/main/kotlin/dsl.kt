c507ccea471a28f85277ad1a70fa77fc9815fcf6
Alexander Koval <kovalidis@gmail.com>
2018-11-08 01:09:43 +0200

    Sub document: SubDocument class

--- src/main/kotlin/dsl.kt
+++ src/main/kotlin/dsl.kt
@@ -23,38 +23,40 @@ abstract class FieldSet {
     }
 }
 
-abstract class Document : FieldSet() {
-    val meta = MetaFields()
-
-    fun <V: Document> obj(factory: () -> V) = SubDocumentProperty(factory)
+abstract class SubDocument : FieldSet() {
+    fun <V: SubDocument> obj(factory: () -> V) = SubDocumentProperty(factory)
 
     class SubFieldsProperty<V: SubFields>(private val factory: () -> V) {
-        operator fun provideDelegate(thisRef: Document, prop: KProperty<*>): ReadOnlyProperty<Document, V> {
+        operator fun provideDelegate(thisRef: SubDocument, prop: KProperty<*>): ReadOnlyProperty<SubDocument, V> {
             println("> SubFieldsProperty.provideDelegate($thisRef, ${prop.name})")
             val subFields = factory().apply {
                 _name = prop.name
                 _qualifiedName = thisRef.calcQualifiedName(_name)
             }
-            return object : ReadOnlyProperty<Document, V> {
-                override fun getValue(thisRef: Document, property: KProperty<*>) = subFields
+            return object : ReadOnlyProperty<SubDocument, V> {
+                override fun getValue(thisRef: SubDocument, property: KProperty<*>) = subFields
             }
         }
     }
 
-    class SubDocumentProperty<V: Document>(private val factory: () -> V) {
-        operator fun provideDelegate(thisRef: Document, prop: KProperty<*>): ReadOnlyProperty<Document, V> {
-            println("> SubDocumentProperty.provideDelegate($thisRef, ${prop.name})")
+    class SubDocumentProperty<V: SubDocument>(private val factory: () -> V) {
+        operator fun provideDelegate(thisRef: SubDocument, prop: KProperty<*>): ReadOnlyProperty<SubDocument, V> {
+//            println("> SubDocumentProperty.provideDelegate($thisRef, ${prop.name})")
             val subDocument by lazy {
                 factory().apply {
                     _name = prop.name
                     _qualifiedName = thisRef.calcQualifiedName(_name)
                 }
             }
-            return object : ReadOnlyProperty<Document, V> {
-                override fun getValue(thisRef: Document, property: KProperty<*>) = subDocument
+            return object : ReadOnlyProperty<SubDocument, V> {
+                override fun getValue(thisRef: SubDocument, property: KProperty<*>) = subDocument
             }
         }
     }
+}
+
+abstract class Document : SubDocument() {
+    val meta = MetaFields()
 }
 
 class Field(val name: String? = null) {
@@ -62,10 +64,10 @@ class Field(val name: String? = null) {
         return "Field(name = $name)"
     }
 
-    fun <V: SubFields> subFields(factory: () -> V) = Document.SubFieldsProperty(factory)
+    fun <V: SubFields> subFields(factory: () -> V) = SubDocument.SubFieldsProperty(factory)
 
     operator fun provideDelegate(thisRef: FieldSet, prop: KProperty<*>): ReadOnlyProperty<FieldSet, BoundField> {
-        println("> Field.provideDelegate($thisRef, ${prop.name})")
+//        println("> Field.provideDelegate($thisRef, ${prop.name})")
         val boundField by lazy {
             val fieldName = name ?: prop.name
             val qualifiedName = thisRef.calcQualifiedName(fieldName)
@@ -101,8 +103,8 @@ object ProductDoc : Document() {
         val sort by field()
     }
 
-    class CompanyDoc : Document() {
-        val name by field()
+    class CompanyDoc : SubDocument() {
+        val name by field().subFields { NameFields() }
     }
 
     val name by field().subFields { NameFields() }
@@ -120,6 +122,6 @@ fun main() {
         .also(::println)
     ProductDoc.company.name
         .also(::println)
-    ProductDoc.company.meta.id
+    ProductDoc.company.name.sort
         .also(::println)
 }
