6b32966fabcda318622d9de512cc24a06f82204a
Alexander Koval <kovalidis@gmail.com>
2018-11-07 23:54:57 +0200

    Basic project

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.3.0"
+}
+
+group = "company.evo"
+version = "0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compile(kotlin("stdlib-jdk8"))
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
---
+++ src/main/kotlin/dsl.kt
@@ -0,0 +1,4 @@
+
+fun main() {
+    println("Let's design some nice dsl")
+}
