0b26b6e845f2fbd920f8e5d2671c8001b45a47a3
Alexander Koval <kovalidis@gmail.com>
2018-11-08 00:46:17 +0200

    WIP Qualified name for sub fields

--- src/main/kotlin/dsl.kt
+++ src/main/kotlin/dsl.kt
@@ -1,13 +1,35 @@
+import java.lang.IllegalStateException
 import kotlin.reflect.KProperty
 
 abstract class FieldSet {
+    var _name = ""
+        set(value) {
+            if (_name.isNotEmpty()) throw IllegalStateException()
+            field = value
+        }
+    var _qualifiedName = ""
+        set(value) {
+            if (_qualifiedName.isNotEmpty()) throw IllegalStateException()
+            field = value
+        }
+
     fun field(name: String? = null) = Field(name)
+
+    fun calcQualifiedName(fieldName: String) = if (_qualifiedName.isNotEmpty()) {
+        "$_qualifiedName.$fieldName"
+    } else {
+        fieldName
+    }
 }
 
 abstract class Document : FieldSet() {
     class SubFieldsProperty<V: SubFields>(private val factory: () -> V) {
         operator fun getValue(thisRef: Document, prop: KProperty<*>): V {
-            return factory()
+            println("> SubFieldsProperty.getValue($thisRef, ${prop.name})")
+            return factory().apply {
+                _name = prop.name
+                _qualifiedName = thisRef.calcQualifiedName(_name)
+            }
         }
     }
 }
@@ -20,13 +42,16 @@ class Field(val name: String? = null) {
     fun <V: SubFields> subFields(factory: () -> V) = Document.SubFieldsProperty(factory)
 
     operator fun getValue(thisRef: FieldSet, prop: KProperty<*>): BoundField {
-        return BoundField(name ?: prop.name)
+        println("> Field.getValue($thisRef, ${prop.name})")
+        val fieldName = name ?: prop.name
+        val qualifiedName = thisRef.calcQualifiedName(fieldName)
+        return BoundField(fieldName, qualifiedName)
     }
 }
 
-class BoundField(val name: String) {
+class BoundField(val name: String, val qualifiedName: String) {
     override fun toString(): String {
-        return "BoundField(name = $name)"
+        return "BoundField(name = $name, qualifiedName = $qualifiedName)"
     }
 }
 
@@ -46,6 +71,8 @@ fun main() {
     println("Let's design some nice dsl")
 
     ProductDoc.name
+        .also(::println)
+    ProductDoc.name.sort
         .also(::println)
     ProductDoc.name.sort
         .also(::println)
